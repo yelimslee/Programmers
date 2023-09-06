4555fa5fd6fbf1753b95a8a998c2439c7c966c76
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-09-06 14:54:46 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,21 +1,16 @@
 def solution(n, computers):
-    # 결과를 담을 변수, 결국 남아있는 네트워크를 여기 저장함.
     count = 0
-    # 실제 방문 여부를 체크할 수 있는 불리언 값을 가진 리스트. 항상 이걸 체크하면서 방문여부를 확인.
-    visited = [False] * n  # [False, False, False]
-    # visited = [False for _ in range(0, n)]
-    
-    def dfs(vertex):  # 수정: 변수 이름이 1이 아니라 vertex여야 함
-        #더 이상 도달할 곳이 없는 경우 -> 단순히 반복이 끝났을 경우
-        visited[vertex] = True
-        
+    visited = [False] * n  # 모든 노드가 방문되지 않은 상태로 초기화된 방문 노드 목록
+
+    def dfs(vertex):
+        visited[vertex] = True  # 현재 노드를 방문한 것으로 표시
         for idx in range(n):
-            if computers[vertex][idx] == 1 and visited[idx] == False:
-                dfs(idx)
-    
-    for vertex in range(n):  # 수정: 변수 이름이 2가 아니라 vertex여야 함
-        if visited[vertex] == False:
-            dfs(vertex)
-            count += 1
-    
-    return count
+            if computers[vertex][idx] == 1 and not visited[idx]:
+                dfs(idx)  # 인접한 미방문 노드를 재귀적으로 방문
+
+    for node in range(n):  # 모든 노드를 반복
+        if not visited[node]:  # 노드가 방문되지 않았다면 새로운 DFS를 시작
+            dfs(node)
+            count += 1  # 새로운 연결된 구성 요소가 발견될 때마다 카운트 증가
+
+    return count  # 연결된 구성 요소의 개수를 반환
