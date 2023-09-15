a58d323d78af6b70132b6187e235f6e7a7066655
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-09-15 14:10:46 +0900

    [D3] Title: 24시간, Time: 245 ms, Memory: 61,452 KB -BaekjoonHub

--- SWEA/D3/12368. 24시간/24시간.py
+++ SWEA/D3/12368. 24시간/24시간.py
@@ -1,13 +1,8 @@
-def twentyfourhours(test_case, now, hour):
-    time = now + hour
-    
-    return "#" + str(test_case) + " " + str(time % 24)
-
-
-if __name__ == "__main__":
-    T = int(input())
-
-    for test_case in range(1, T + 1):
-        now, hour = map(int, input().split())
-
-        print(twentyfourhours(test_case, now, hour))
+T = int(input())
+ 
+for i in range(1, T+1):
+    A, B = map(int, input().split())
+  
+    result = (A + B) % 24
+     
+    print(f"#{i} {result}")
