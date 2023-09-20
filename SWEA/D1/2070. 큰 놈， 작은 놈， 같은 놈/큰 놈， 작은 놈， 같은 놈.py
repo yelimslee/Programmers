548b5bb499425ac8dfd62177bea3082e1fd58a7f
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-09-20 13:07:54 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 151 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
@@ -0,0 +1,9 @@
+t = int(input())
+for i in range(1, t+1):
+    a, b = map(int, input().split())
+    if a > b:
+        print(f"#{i} >")
+    elif a == b:
+        print(f"#{i} =")
+    elif a < b:
+        print(f"#{i} <")
