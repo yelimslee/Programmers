6318146d411d27b1ef0c6343408dd0d900574f5d
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-09-24 16:17:07 +0900

    [D2] Title: Base64 Decoder, Time: 189 ms, Memory: 58,240 KB -BaekjoonHub

---
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -0,0 +1,26 @@
+decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+      'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+      '0','1','2','3','4','5','6','7','8','9','+','/'
+      ]
+
+T = int(input())
+
+for i in range(1, T+1):
+    word = list(input())
+    value = ""
+
+    for j in range(len(word)):
+        num = decode.index(word[j])
+        bin_num = str(bin(num)[2:])
+
+        while len(bin_num) < 6:
+            bin_num = "0" + bin_num
+        value += bin_num
+
+    result = ""
+    for j in range(len(word) * 6 // 8):
+        data = int(value[j * 8 : j * 8 + 8], 2)
+        result += chr(data)
+
+
+    print(f"#{i} {result}")
