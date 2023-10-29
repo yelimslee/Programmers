07be2ca3df4584509fa91338021c2dcfec9479f6
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-10-29 21:41:57 +0900

    Update 백만 장자 프로젝트.py

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -4,22 +4,22 @@
 
 # 뒤에서 순회, O(n)
 # 첫인덱스: max, 그 다음 수가 작으면 max-list[i]만큼 누적, 크면 max갱신
+import sys
+sys.stdin = open("input.txt", "r")
+
 T = int(input())
-for test_case in range(1, T + 1):
+for test_case in range(1, T+1):
     N = int(input())
-    li = list(map(int, input().split()))
+    lst = list(map(int, input().split()))[::-1]
 
-    # 뒤에서 부터 편하게 탐색하기 위해 문자열 전체 역순화
-    li = li[::-1]
-    profit = 0
-    curr = li[0]
-    for i in range(1, N):
-        if curr > li[i]:
-            profit += (curr - li[i])
+    profit = max = 0
+    for i in lst:
+        if max < i:
+            max = i
         else:
-            curr = li[i]
+            profit += (max - i)
 
-    print(f'#{test_case} {profit}')
+    print(f"#{test_case} {profit}")
 
 
 
