210851c8fad780a8da61379c97f4250c26f56424
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-01 22:05:47 +0900

    [D2] Title: 간단한 369게임, Time: 149 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,16 +1,18 @@
-T = int(input())
+# 10으로 나눴을 때, 3 6 9면 박수
+
+N = int(input())
 ans = ""
-for test_case in range(1, T+1):
-    now = test_case
-    yes = 0  # 각 자리수가 3,6,9 숫자인 경우 개수를 셈
-    no = 0  # 각 자리수가 3, 6, 9를 제외한 숫자인 경우를 셈
-    while test_case >= 1:
-        x = test_case % 10
-        if x == 3 or x == 6 or x == 9:
+for i in range(1, N+1):
+    now = i
+    yes = 0  # 각 자릿수가 3,6,9 숫자인 경우의 갯수를 셈
+    no = 0  # 각 자릿수가 3,6,9 를 제외한 숫자인 경우를 셈
+    while i >= 1:
+        r = i % 10  # 10으로 나눈 나머지
+        if r == 3 or r == 6 or r == 9:
             yes += 1
         else:
             no += 1
-        test_case = test_case // 10  # 몫 연산을 통해 다음 연산할 숫자 갱신
+        i = i//10  # 몫 연산을 통해 다음 연산할 숫자를 갱신
 
     if yes == 0:
         ans += str(now)
