9bddbf12e17ece2c912cff00ee1d1b9719383e98
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-02 21:21:52 +0900

    Update ［S／W 문제해결 기본］ 1일차 － Flatten.py

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -15,3 +15,19 @@ for test_case in range(1, T+1):
         dump -= 1
 
     print(f"#{test_case} {max(box) - min(box)}")
+
+
+
+# 1. 정렬(오름차순)  2. lst[0]+=1, lst[-1]-=1
+T = 10
+for test_case in range(1, T+1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    
+    for _ in range(N):
+        lst.sort()
+        lst[0] += 1
+        lst[-1] -= 1
+
+    
+    print(f"#{test_case} {max(lst)-min(lst)}")
