7d81e2b9c61362b5236287a174318e45aaf87b1a
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-02 22:13:56 +0900

    [D2] Title: 달팽이 숫자, Time: 154 ms, Memory: 58,248 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,24 +1,23 @@
 T = int(input())
-di = [0, -1, 0, 1]
-dj = [1, 0, -1, 0]
+di = [0,1,0,-1]
+dj = [1,0,-1,0]
 for test_case in range(1, T+1):
     N = int(input())
     arr = [[0] * N for _ in range(N)]
 
-    i, j, cnt, dr = 0, 0, 1, 0  # 초기화
+    i, j, cnt, dr = 0, 0, 1, 0
     arr[i][j] = cnt
     cnt += 1
 
     while cnt <= N*N:
         ni, nj = i+di[dr], j+dj[dr]
-        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 0:
-            i, j = ni, nj
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            i,j=ni,nj
             arr[i][j] = cnt
             cnt += 1
         else:
-            dr = (dr+1) % 4
+            dr = (dr+1)%4
 
     print(f"#{test_case}")
-    # 1차원은 리스트, 2차원은 배열
     for lst in arr:
         print(*lst)
