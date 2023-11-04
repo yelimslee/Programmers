1f268aab9e8cec7daff536bcb02121439e103052
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-05 00:16:26 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 153 ms, Memory: 61,208 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,3 +1,6 @@
+# 딱 3개의 1이 연속일 경우
+# 0인 경우 -> cnt==0 or 마지막 열 (맨 오른쪽열과 마지막행 0으로 패딩)
+# 세로의 경우: 전체 행렬
 def count(arr):
     ret = 0
     for lst in arr:
@@ -8,13 +11,15 @@ def count(arr):
             else:
                 if cnt == K:
                     ret += 1
-                cnt = 0
+                cnt = 0  
     return ret
 
 T = int(input())
 for test_case in range(1, T+1):
     N, K = map(int, input().split())
-    arr = [list(map(int, input().split())) + [0] for _ in range(N)] + [[0]*(N+1)]
+    # 행으로 처리
+    arr = [list(map(int, input().split())) +[0] for _ in range(N)] + [[0]*(N+1)]
+    # 열로 처리
     arr_t = list(map(list, zip(*arr)))
 
     ans = count(arr) + count(arr_t)
