2b75f93dabebba5f95ccde45df946df7d72bea21
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-04 22:55:11 +0900

    [D2] Title: Base64 Decoder, Time: 147 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -1,26 +1,32 @@
-decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+# dic 리스트에 인코딩 값 넣기
+dic = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
       'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-      '0','1','2','3','4','5','6','7','8','9','+','/'
-      ]
+      '0','1','2','3','4','5','6','7','8','9','+','/' ]
 
 T = int(input())
-
-for i in range(1, T+1):
+for test_case in range(1, T + 1):
     word = list(input())
-    value = ""
-
+    value = '' 
+    
     for j in range(len(word)):
-        num = decode.index(word[j])
+    
+        # num 변수에 dic 리스트 인덱스값 넣기
+        num = dic.index(word[j])
+        
+        # 2진수로 변환하고 맨 앞의 '0b'를 제거하기 위해 2번 인덱스부터 저장하기
         bin_num = str(bin(num)[2:])
-
+        
+        # bin_num 값 빈 자리를 0으로 채우기
         while len(bin_num) < 6:
-            bin_num = "0" + bin_num
+            bin_num = '0' + bin_num
         value += bin_num
-
-    result = ""
-    for j in range(len(word) * 6 // 8):
-        data = int(value[j * 8 : j * 8 + 8], 2)
-        result += chr(data)
-
-
-    print(f"#{i} {result}")
+            
+    result = ''
+    
+    # 10진수로 변환하기 위해 8자리로 만들어줌 (입력값*6) // 8
+    for k in range(len(word)*6 // 8):
+    
+        # 10진수로 변환
+        number = int(value[k*8 : k*8+8], 2)
+        result += chr(number)
+    print('#%d %s' % (test_case, result))
