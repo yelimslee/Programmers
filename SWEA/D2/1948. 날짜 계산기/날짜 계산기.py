9a18a79243e86c431d578d729ce88b514b2bdbf5
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-05 04:45:32 +0900

    [D2] Title: 날짜 계산기, Time: 155 ms, Memory: 58,508 KB -BaekjoonHub

---
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -0,0 +1,19 @@
+months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+for test_case in range(1, int(input())+1):
+    m1, d1, m2, d2 = map(int, input().split())
+    ans = 0
+
+    # 같은 달에 위치할 경우
+    if m1 == m2:
+        ans = d2 - d1 + 1
+
+    else:
+        # 시작하는 달
+        ans = months[m1] - d1 + 1
+        # 중간에 있는 달
+        for i in range(m1 + 1, m2):
+            ans += months[i]
+        # 마지막 달
+        ans += d2
+    print(f'#{test_case} {ans}')
