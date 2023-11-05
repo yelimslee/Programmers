92d8f8a53c69cb4b8a0a4c09cbc00997c4f9dc1c
yelimslee <141470772+yelimslee@users.noreply.github.com>
2023-11-05 23:57:18 +0900

    [D2] Title: 가랏! RC카!, Time: 148 ms, Memory: 58,492 KB -BaekjoonHub

---
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -0,0 +1,17 @@
+T = int(input())
+for test_case in range(1, T+1):
+    n = int(input())
+    speed = 0
+    move = 0
+    for _ in range(n):
+        arr = list(map(int, input().split()))
+        if arr[0] == 1:
+            speed += arr[1]
+        elif arr[0] == 2:
+            if speed > arr[1]: 
+                speed -= arr[1]
+            else:
+                speed = 0
+        move += speed
+    
+    print(f"#{test_case} {move}")
